c5548bcabe345904dfffbe0c0d29d82ae65ff496
Dorian Mejer <dorian.mejer@gmail.com>
2019-11-25 18:33:48 +0100

    fish startup script

---
+++ .config/fish/config.fish
@@ -0,0 +1,10 @@
+nvm
+
+abbr g 'git'
+abbr gco 'git checkout'
+abbr gcom 'git checkout master'
+abbr gs 'git stash'
+abbr gsa 'git stash apply'
+abbr gl 'git pull'
+abbr gm 'git merge'
+abbr gp 'git push'
