566e759e5994b3ecfe1e42fda779dbf6c9d5a46e
Dorian Mejer <dorian.mejer@gmail.com>
2020-05-29 21:54:58 +0200

    polybar

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -7,6 +7,6 @@ killall -q polybar
 
 # Launch bar1 
 echo "---" | tee -a /tmp/polybar1.log
-polybar bar1 >>/tmp/polybar1.log 2>&1 &
+polybar -r bar1 >>/tmp/polybar1.log 2>&1 &
 
 echo "Bars launched..."
